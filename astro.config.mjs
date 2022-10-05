a2ed58ed37750a7c794d5063a56694c3a26b7a8a
oussama.aabida <oussama.aabida@disway.com>
2022-10-05 15:09:46 +0100

    My site ready for deployment.

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://astro-moon-landing.netlify.app/",
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      external: ["@11ty/eleventy-img", "svgo"],
+    },
+  },
+  outDir: './test',
+  output: 'static'
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+      },
+      backgroundImage: {
+        'Test-Image': "url(./src/assets/astronaut.png)"
+      }
+    },
+  },
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [require("tailwindcss-fluid-type")],
+};
